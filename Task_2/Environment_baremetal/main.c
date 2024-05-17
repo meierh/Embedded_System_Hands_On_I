5d064526c4ad2ca91d644e0f82f42f98cd14776e
Tobias Henig <tobias.henig@stud.tu-darmstadt.de>
2024-05-17 16:43:25 +0200

    first try to turn on LED

--- Task_2/Environment_baremetal/main.c
+++ Task_2/Environment_baremetal/main.c
@@ -5,10 +5,24 @@
  *
  */
 
+// include CMSIS library for our Cortex-M0 processor
+#include "stm32f030xc.h"
+
 int main(void) {
+	/*
 	unsigned int i = 0;
 	while (1) {
 		i ++;
 	}
+	*/
+
+	// Set mode register of port A pin 7 to 0b10 => General purpose output
+	GPIOA->MODER = 0;
+	GPIOA->MODER |= GPIO_MODER_MODER7_0;
+
+	// Set output register of port A pin 7 to 1
+	GPIOA->ODR = 0;
+	GPIOA->ODR |= GPIO_ODR_7; 
+
 	return 0;
 }
