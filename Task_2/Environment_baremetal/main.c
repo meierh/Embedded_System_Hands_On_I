3abf414855d1331a2a2fa81101f04476b71f7eeb
Helge Meier <hmelbtal@t-online.de>
2024-05-17 18:12:21 +0200

    Corrected LED set

--- Task_2/Environment_baremetal/main.c
+++ Task_2/Environment_baremetal/main.c
@@ -9,20 +9,16 @@
 #include "stm32f030xc.h"
 
 int main(void) {
-	/*
-	unsigned int i = 0;
-	while (1) {
-		i ++;
-	}
-	*/
 
 	// Set mode register of port A pin 7 to 0b10 => General purpose output
-	GPIOA->MODER = 0;
-	GPIOA->MODER |= GPIO_MODER_MODER7_0;
+	uint32_t* GPIOA_MODER_Reg =  GPIOA_BASE + GPIO_TypeDef.MODER;
+	*GPIOA_MODER_Reg |=  ((uint32_t)1 << GPIO_MODER_MODER7_0 /*14*/); //set bit 14 to 1
+	*GPIOA_MODER_Reg &= ~((uint32_t)1 << GPIO_MODER_MODER7_1 /*15*/); //set bit 15 to 0
 
 	// Set output register of port A pin 7 to 1
-	GPIOA->ODR = 0;
-	GPIOA->ODR |= GPIO_ODR_7; 
+	uint32_t* GPIOA_ODR =  GPIOA_BASE + GPIO_TypeDef.ODR;
+	*GPIOA_ODR |=  ((uint32_t)1 << GPIO_ODR_7); //set bit 7 to 1
+	// why is GPIO_ODR_7 = 8???
 
 	return 0;
 }
