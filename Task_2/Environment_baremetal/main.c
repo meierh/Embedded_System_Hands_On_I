69c0f231c1dadd88b9ec35923de24454e92ac5a4
Nicolas Kruppa <nicolas.kruppa@stud.tu-darmstadt.de>
2024-05-18 14:14:05 +0200

    fix led code

--- Task_2/Environment_baremetal/main.c
+++ Task_2/Environment_baremetal/main.c
@@ -1,24 +1,24 @@
-/*
- * Dummy main source file.
- *
- * Please add apropriate include file
- *
- */
-
 // include CMSIS library for our Cortex-M0 processor
 #include "stm32f030xc.h"
+#include <stdint.h>
+
+#define LED_PIN    (7)
 
 int main(void) {
 
-	// Set mode register of port A pin 7 to 0b10 => General purpose output
-	uint32_t* GPIOA_MODER_Reg =  GPIOA_BASE + GPIO_TypeDef.MODER;
-	*GPIOA_MODER_Reg |=  ((uint32_t)1 << GPIO_MODER_MODER7_0 /*14*/); //set bit 14 to 1
-	*GPIOA_MODER_Reg &= ~((uint32_t)1 << GPIO_MODER_MODER7_1 /*15*/); //set bit 15 to 0
+	// Enable the GPIOA peripheral in 'RCC_AHBENR'.
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
 
-	// Set output register of port A pin 7 to 1
-	uint32_t* GPIOA_ODR =  GPIOA_BASE + GPIO_TypeDef.ODR;
-	*GPIOA_ODR |=  ((uint32_t)1 << GPIO_ODR_7); //set bit 7 to 1
-	// why is GPIO_ODR_7 = 8???
+	// A7 is connected to an LED
+	// It should be set to push-pull low-speed output.
+	// reset the register with 00
+	GPIOA->MODER  &= ~(0x3 << (LED_PIN*2));
+	// set the register to output mode
+	GPIOA->MODER  |= (0x1 << (LED_PIN*2));
+	// set output type register to outout open-drain
+	GPIOA->OTYPER &= ~(1 << LED_PIN);
+	// enable the LED
+	GPIOA->ODR |= (1 << LED_PIN);
 
 	return 0;
 }
