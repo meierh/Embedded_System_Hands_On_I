027d9f20d35e20929aae43eec9c424282fc780aa
Tobias Henig <tobias.henig@stud.tu-darmstadt.de>
2024-06-05 20:18:46 +0200

    update main to turn on all leds

--- Task_2/Environment_baremetal/main.c
+++ Task_2/Environment_baremetal/main.c
@@ -1,24 +1,34 @@
 // include CMSIS library for our Cortex-M0 processor
-#include "stm32f030xc.h"
+//#include "stm32f030xc.h"
+#define STM32F030xC
+#include "stm32f0xx.h"
 #include <stdint.h>
 
-#define LED_PIN    (7)
+#define LED0_PIN    (7)
+#define LED1_PIN	(0)
+#define LED2_PIN	(1)
 
 int main(void) {
 
-	// Enable the GPIOA peripheral in 'RCC_AHBENR'.
-	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+	// Enable the GPIOA and GPIOB peripherals in 'RCC_AHBENR'.
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN | RCC_AHBENR_GPIOBEN;
 
 	// A7 is connected to an LED
 	// It should be set to push-pull low-speed output.
 	// reset the register with 00
-	GPIOA->MODER  &= ~(0x3 << (LED_PIN*2));
+	GPIOA->MODER  &= ~(0x3 << (LED0_PIN*2));
 	// set the register to output mode
-	GPIOA->MODER  |= (0x1 << (LED_PIN*2));
+	GPIOA->MODER  |= (0x1 << (LED0_PIN*2));
 	// set output type register to outout push-pull
-	GPIOA->OTYPER &= ~(1 << LED_PIN);
+	GPIOA->OTYPER &= ~(1 << LED0_PIN);
 	// enable the LED
-	GPIOA->ODR |= (1 << LED_PIN);
+	GPIOA->ODR |= (1 << LED0_PIN);
+
+	// Turn on the other two LEDs the same way
+	GPIOB->MODER &= ~(0x3 << (LED1_PIN*2) | 0x3 << (LED2_PIN*2));
+	GPIOB->MODER |= (0x1 << (LED1_PIN*2) | 0x1 << (LED2_PIN*2));
+	GPIOB->OTYPER &= ~(1 << LED1_PIN | 1 << LED2_PIN);
+	GPIOB->ODR |= (1 << LED1_PIN) | (1 << LED2_PIN);
 
 	return 0;
 }
