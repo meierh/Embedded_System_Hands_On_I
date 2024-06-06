bef4038d86d9e84c551e628bc54df3b0373ebc38
Tobias Henig <tobias.henig@stud.tu-darmstadt.de>
2024-06-06 18:15:35 +0200

    rename main.c to test_leds.c and add some comments

--- Task_2/Environment_baremetal/test_leds.c
+++ Task_2/Environment_baremetal/test_leds.c
@@ -1,8 +1,11 @@
 // include CMSIS library for our Cortex-M0 processor
-//#include "stm32f030xc.h"
 #define STM32F030xC
 #include "stm32f0xx.h"
-#include <stdint.h>
+//#include "stm32f030xc.h" // (alternative, cf. README)
+
+/*
+This is just a very basic program which turns on all LEDs on the ESA extension board
+*/
 
 #define LED0_PIN    (7)
 #define LED1_PIN	(0)
@@ -10,11 +13,12 @@
 
 int main(void) {
 
-	// Enable the GPIOA and GPIOB peripherals in 'RCC_AHBENR'.
+	// Clock-enable the GPIOA and GPIOB peripherals in 'RCC_AHBENR'.
 	RCC->AHBENR |= RCC_AHBENR_GPIOAEN | RCC_AHBENR_GPIOBEN;
 
-	// A7 is connected to an LED
-	// It should be set to push-pull low-speed output.
+	// A7 is connected to LED0
+	// This pin should be set to push-pull low-speed output.
+
 	// reset the register with 00
 	GPIOA->MODER  &= ~(0x3 << (LED0_PIN*2));
 	// set the register to output mode
