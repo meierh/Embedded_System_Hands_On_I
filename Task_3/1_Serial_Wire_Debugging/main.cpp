13161a70e83e48f301948d3ffceb7e5ae5b041e2
Tobias Henig <tobias.henig@stud.tu-darmstadt.de>
2024-07-11 23:18:20 +0200

    add stdout of IDCODE and delete untested read/write calls

--- Task_3/1_Serial_Wire_Debugging/main.cpp
+++ Task_3/1_Serial_Wire_Debugging/main.cpp
@@ -7,8 +7,5 @@ int main(int argc, char *argv[])
     std::bitset<32> IDCODE;
     instance.readIDCODE(IDCODE);
     
-    instance.write(AccessRegister::DebugPort,0b10,0x12345678);
-
-    std::bitset<32> rData;
-    instance.read(AccessRegister::DebugPort,0b11,rData);
+    printf("IDCODE = %#010x\n", IDCODE.to_ulong());
 }
