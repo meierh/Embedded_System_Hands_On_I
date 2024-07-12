bf24618db05f665a92b8dd01143f4cff3923aac5
Helge_Meier <hmelbtal@t-online.de>
2024-07-12 16:46:11 +0200

    Changed FIR Filter to microcontroller settings

--- Task_5/4_Realization_of_Digital_Filters/FIRFilter.h
+++ Task_5/4_Realization_of_Digital_Filters/FIRFilter.h
@@ -1,23 +1,49 @@
 #ifndef FIRFILTER_H
 #define FIRFILTER_H
 
-#include <stdio.h>
+#include <stdint.h>
 
-typedef struct
+template<typename Floating, uint8_t ORDER>
+class FIRFilter
 {
-    unsigned int N;
-    unsigned int k;
-    float* a;
-    float* x;
-} FIRFilter;
+    public:
+        FIRFilter(Floating a[ORDER+1]):k(0)
+        {
+            for(int i=0; i<ORDER+1; i++)
+                this->a[i] = a[i];
+        }
+        
+        Floating operator()(Floating x)
+        {
+            rightShiftMem();
+            this->x[0] = x;
+            incr();
+            return innerMuliplication();
+        }
+        
+    private:
+        Floating a[ORDER+1];
+        Floating x[ORDER+1];
+        unsigned int k;
+        
+        void rightShiftMem()
+        {
+            for(int ki=ORDER-1; ki>=0; ki--)
+                this->x[ki+1] = this->x[ki];
+        }
+        
+        void insert(Floating x){ this->x[0]=x; }
+        
+        void incr(){ k = (k<(ORDER+1))?++k:ORDER+1; }
+        
+        Floating innerMuliplication()
+        {
+            Floating y = 0;
+            for(unsigned int i=0; i<k; i++)
+                y += a[i]*this->x[i];
+            return y;
+        }
+};
 
-// a must be a memory of size N+1, a is read only, allocates memory
-void initialize(FIRFilter* filter, unsigned int N, const float* a);
-
-// frees memory if allocated
-void close(FIRFilter* filter);
-
-// input value and output filtered value, filter must be initialized
-float filterValue(FIRFilter* filter, float x);
 
 #endif
