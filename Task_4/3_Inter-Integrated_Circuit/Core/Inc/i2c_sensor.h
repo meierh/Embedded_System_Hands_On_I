e845a3278909078aa511be9f65867d99a1976246
Nicolas Kruppa <nicolas.kruppa@stud.tu-darmstadt.de>
2024-07-17 00:06:05 +0200

    fix i2c communication

--- Task_4/3_Inter-Integrated_Circuit/Core/Inc/i2c_sensor.h
+++ Task_4/3_Inter-Integrated_Circuit/Core/Inc/i2c_sensor.h
@@ -19,43 +19,90 @@ public:
 
     }
 
-    template<uint16_t sizeTemp>
-    void write(array<sizeTemp>& data) {
-        //HAL_I2C_Master_Transmit(&hi2c1, address, data.data(), data.size(), 1000);
-        //HAL_I2C_Master_Transmit_IT(&hi2c1, address, data.data(), data.size());
+    static bool checkStateOfI2c() {
+        // check state of i2c
+        HAL_I2C_StateTypeDef i2c_state = HAL_I2C_GetState(&hi2c1);
+
+        if(i2c_state != HAL_I2C_STATE_READY) {
+            // i2c not ready
+            return false;
+        } else {
+            return true;
+        }
     }
 
-    template<uint16_t sizeTemp>
-    void read(array<sizeTemp>& data) {
-        HAL_I2C_Master_Receive(&hi2c1, address, data.data(), data.size(), 1000);
-        //HAL_I2C_Master_Receive_IT(&hi2c1, address, data.data(), data.size());
-    }
-
-    void writeVector(std::vector<uint8_t>&  data) {
-        //HAL_I2C_Master_Transmit(&hi2c1, address, data.data(), data.size(), 1000);
-        //HAL_I2C_Master_Transmit_IT(&hi2c1, address, data.data(), data.size());
-
-        while(HAL_I2C_Mem_Write(&hi2c1, address, 0x0F, 1, data.data(), 1,10000) != HAL_OK);
-    }
-
-
-    void readVector(std::vector<uint8_t>& data) {
-        uint16_t newAddress = 0xb9;
-
-        HAL_StatusTypeDef state = HAL_I2C_IsDeviceReady(&hi2c1, newAddress, 5, 1000);
+    static bool checkDeviceStatus(uint16_t readAddress) {
+        // check if device is ready
+        HAL_StatusTypeDef state = HAL_I2C_IsDeviceReady(&hi2c1, readAddress, 5, 1000);
 
         if(state != HAL_OK) {
-            bool error = 1;
-            uint32_t errorCode = HAL_I2C_GetError(&hi2c1);
-            Error_Handler();
-        }
-
-        if(HAL_I2C_Mem_Read(&hi2c1, newAddress, 0x0F, I2C_MEMADD_SIZE_8BIT, data.data(), 1, 1000) != HAL_OK) {
-            Error_Handler();
+            //uint32_t errorCode = HAL_I2C_GetError(&hi2c1);
+            return false;
+        }else  {
+            return true;
         }
     }
 
 
+    void writeVector(std::vector<uint8_t>&  data, std::vector<uint8_t>&msg) const {
+        // to write to the device, we have to shift the 7 bit address to a 8 bit address. The last bit is 0 for write
+        uint16_t readAddress = address << 1;
+
+        if(!checkStateOfI2c()) {
+            return;
+        }
+
+        if(!checkDeviceStatus(readAddress)){
+            return;
+        }
+
+        if(HAL_I2C_Mem_Write(&hi2c1, readAddress, msg.at(0), I2C_MEMADD_SIZE_8BIT, &data.at(0), 1, 10000) != HAL_OK) {
+            // @todo improve error handling
+            uint32_t errorCode = HAL_I2C_GetError(&hi2c1);
+            data.push_back(errorCode);
+            Error_Handler();
+        }
+    }
+
+    void readVector(std::vector<uint8_t>& data, std::vector<uint8_t>& msg, uint16_t size) const {
+        // to read from the device, we have to shift the 7 bit address to a 8 bit address and set the last bit to 1 for reading
+        uint16_t readAddress = ((address << 1) |0x1);
+
+        if(!checkStateOfI2c()) {
+            return;
+        }
+
+        if(!checkDeviceStatus(readAddress)){
+            return;
+        }
+
+        // read the mem value and push the result to the data vector
+        uint8_t pData[size];
+        if(HAL_I2C_Mem_Read(&hi2c1, readAddress, msg.at(0), I2C_MEMADD_SIZE_8BIT, pData, size, 10000) != HAL_OK) {
+            //@todo improve error handling
+            uint32_t errorCode = HAL_I2C_GetError(&hi2c1);
+            data.push_back(errorCode);
+            Error_Handler();
+        }
+        // push every value of the buffer to the vector
+        uint8_t size_of_data = sizeof(pData);
+        for(int i=0; i< size_of_data; i++) {
+            data.push_back(pData[i]);
+        };
+    }
+
+
+    /* i2c Error Codes copies from stm32f0xx_hal_i2c.h
+    * #define HAL_I2C_ERROR_NONE      (0x00000000U)       !< No error
+    * #define HAL_I2C_ERROR_BERR      (0x00000001U)    !< BERR error
+    * #define HAL_I2C_ERROR_ARLO      (0x00000002U)    !< ARLO error
+    * #define HAL_I2C_ERROR_AF        (0x00000004U)    !< ACKF error
+    * #define HAL_I2C_ERROR_OVR       (0x00000008U)    !< OVR error
+    * #define HAL_I2C_ERROR_DMA       (0x00000010U)    !< DMA transfer error
+    * #define HAL_I2C_ERROR_TIMEOUT   (0x00000020U)    !< Timeout error
+    * #define HAL_I2C_ERROR_SIZE      (0x00000040U)    !< Size Management error
+    * #define HAL_I2C_ERROR_DMA_PARAM (0x00000080U)    !< DMA Parameter Error
+    */
 
 };
 
