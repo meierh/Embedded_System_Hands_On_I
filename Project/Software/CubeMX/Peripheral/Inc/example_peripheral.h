9dbebed4fc70545da9b09b1824dd84cd7556711b
Tobias Henig <tobias.henig@stud.tu-darmstadt.de>
2024-09-16 04:39:05 +0200

    add Peripheral folder

---
+++ Project/Software/CubeMX/Peripheral/Inc/example_peripheral.h
@@ -0,0 +1,18 @@
+//
+// Created by Tobias Henig on 16.09.24.
+//
+
+#ifndef EXAMPLE_PERIPHERAL_H
+#define EXAMPLE_PERIPHERAL_H
+
+
+
+class example_peripheral {
+
+public:
+    int answer = 42;
+};
+
+
+
+#endif //EXAMPLE_PERIPHERAL_H
