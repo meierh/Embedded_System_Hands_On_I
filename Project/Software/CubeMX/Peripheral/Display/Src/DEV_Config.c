d937552f9c2099621256bbc180c52f782a168d7e
Nicolas Kruppa <nicolas.kruppa@stud.tu-darmstadt.de>
2024-09-19 11:35:46 +0200

    comment out exported hi2c1

--- Project/Software/CubeMX/Peripheral/Display/Src/DEV_Config.c
+++ Project/Software/CubeMX/Peripheral/Display/Src/DEV_Config.c
@@ -15,7 +15,7 @@
 #include <stdlib.h>
 // #include "usart.h"
 
-extern I2C_HandleTypeDef hi2c1;
+// extern I2C_HandleTypeDef hi2c1;
 
 /********************************************************************************
 function:	System Init
