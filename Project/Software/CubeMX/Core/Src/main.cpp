09d57247d05e5427d8e7bd660a680fb8259dbc4a
Tobias Henig <tobias.henig@stud.tu-darmstadt.de>
2024-09-23 20:39:43 +0200

    increase EXTI[9:5] preemption priority

--- Project/Software/CubeMX/Core/Src/main.cpp
+++ Project/Software/CubeMX/Core/Src/main.cpp
@@ -485,7 +485,7 @@ static void MX_GPIO_Init(void)
     HAL_GPIO_Init(Rotary_Encoder_A_GPIO_Port, &GPIO_InitStruct);
 
     /* EXTI interrupt init*/
-    HAL_NVIC_SetPriority(EXTI9_5_IRQn, 2, 0);
+    HAL_NVIC_SetPriority(EXTI9_5_IRQn, 0, 0);
     HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
 
     HAL_NVIC_SetPriority(EXTI15_10_IRQn, 3, 0);
