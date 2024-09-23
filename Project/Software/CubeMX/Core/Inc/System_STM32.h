e1fc91ce5e05300d5d3798c9b0eb9f5f7017d3e6
Tobias Henig <tobias.henig@stud.tu-darmstadt.de>
2024-09-23 18:43:02 +0200

    implement interface to DFPlayer

--- Project/Software/CubeMX/Core/Inc/System_STM32.h
+++ Project/Software/CubeMX/Core/Inc/System_STM32.h
@@ -7,27 +7,38 @@
 #include "OLED_1in5.h"
 #include "DS3231.h"
 #include "main.h"
-
+#include "DFP_Controller.h"
 
 class System_STM32 : public System
 {
-    public:
-        System_STM32();
-        ~System_STM32() override;
-        
-        // Application -> System
-        void buttonModePress() override;
-        void displayImage(std::vector<DisplayItem> image) override;
-        DateTime getSystemTime() override;
-        int8_t getSeconds() override;
-        void setSystemTime(DateTime newTime) override;
+public:
+    System_STM32();
+    ~System_STM32() override = default;
 
-        uint8_t getBattery() override;
+    // Application -> System
+    void buttonModePress() override;
+    void displayImage(std::vector<DisplayItem> image) override;
+    DateTime getSystemTime() override;
+    int8_t getSeconds() override;
+    void setSystemTime(DateTime newTime) override;
 
-        void playerTurnOn() override;
-        void playerTurnOff() override;
-        void playerControl(PlayerAction action) override;
-        void playerGetState(PlayingStatus &status, uint8_t &volume, uint16_t &numberOfTracks, uint16_t &currentTrack) override;
+    uint8_t getBattery() override;
+
+    void playerTurnOn() override;
+    void playerTurnOff() override;
+    void playerControl(PlayerAction action) override;
+    void playerGetState(PlayingStatus& status, uint8_t& volume, uint16_t& numberOfTracks,
+                        uint16_t& currentTrack) override;
+
+private:
+    enum class DFP_Mode {Uninitialized, Player, Alarm};
+
+    DFP_Controller mp3Player;
+    DFP_Mode mp3PlayerMode;
+
+    void playerControlPlayerMode(PlayerAction action);
+    void playerControlAlarmMode(PlayerAction action);
+    void playerControlUninitialized(PlayerAction action);
 
 };
 #endif
